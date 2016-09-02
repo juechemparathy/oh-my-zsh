d7a5261f7095126b65e789fdfde802da76b2176c
Jue <jue@yahoo-inc.com>
2016-09-02 16:36:47 -0700

    add personal plugin

---
+++ plugins/playground/playground.plugin.zsh
@@ -0,0 +1,27 @@
+
+#
+# Personal Aliases
+# (sorted alphabetically)
+#
+
+#Samples
+# alias g='git'
+
+# ggu() {
+#   [[ "$#" != 1 ]] && local b="$(git_current_branch)"
+#   git pull --rebase origin "${b:=$1}"
+# }
+# compdef _git ggu=git-checkout
+
+
+#100s git lcoations
+alias 100='cd ~/Playground/Video/master/videosdk-android-demo/videosdk-android-demo'
+
+
+#200 unix comands
+
+
+#300 gradle commands
+
+
+#400 android commands
